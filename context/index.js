e263c788299e86389886885c4bc1416a33670751
Silvin Pradhan <silvinpradhan95@gmail.com>
2022-02-16 23:11:27 -0600

    effect callback to get Csrf token from the backend and match with the generated token from csrf headers

--- context/index.js
+++ context/index.js
@@ -58,6 +58,14 @@ const Provider = ({ children }) => {
     }
   );
 
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const { data } = await axios.get("/api/csrf-token");
+      axios.defaults.headers["X-CSRF-Token"] = data.getCsrfToken;
+    };
+    getCsrfToken();
+  }, []);
+
   return (
     <Context.Provider value={{ state, dispatch }}>{children}</Context.Provider>
   );
