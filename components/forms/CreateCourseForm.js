f718539a2a196e5053d7d89b77b2a54ffd1df4cc
Silvin Pradhan <silvinpradhan95@gmail.com>
2022-03-13 16:07:01 -0500

    moved CreateCourseForm to a separate component

---
+++ components/forms/CreateCourseForm.js
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from "react";
+import { Select, Button } from "antd";
+const { Option } = Select;
+
+const CreateCourseForm = ({
+  handleChange,
+  handleImage,
+  handleSubmit,
+  values,
+  setValues,
+}) => {
+  const children = [];
+  for (let i = 9.99; i <= 100.99; i++) {
+    children.push(<Option key={i.toFixed(2)}>${i.toFixed(2)}</Option>);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group pb-3">
+        <input
+          type={"text"}
+          name="name"
+          className="form-control"
+          placeholder="Name"
+          value={values.name}
+          onChange={handleChange}
+        ></input>
+      </div>
+      <div className="form-group pb-3">
+        <textarea
+          name="description"
+          cols="7"
+          placeholder="Add Course Description..."
+          rows="7"
+          value={values.description}
+          className="form-control"
+          onChange={handleChange}
+        ></textarea>
+      </div>
+      <div className="form-row pb-3">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="form-group">
+              <Select
+                value={values.paid}
+                size="large"
+                style={{ width: "100%" }}
+                onChange={(v) => setValues({ ...values, paid: !values.paid })}
+              >
+                <Option value={true}>Paid</Option>
+                <Option value={false}>Free</Option>
+              </Select>
+            </div>
+          </div>
+          {values.paid && (
+            <div className="col-md-6">
+              <div className="form-group">
+                <Select
+                  defaultValue="$9.99"
+                  style={{ width: "100%" }}
+                  onChange={(v) => setValues({ ...values, price: v })}
+                  tokenSeparators={[,]}
+                  size="large"
+                >
+                  {children}
+                </Select>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="form-group pb-3">
+        <input
+          type={"text"}
+          name="category"
+          className="form-control"
+          placeholder="Category"
+          value={values.category}
+          onChange={handleChange}
+        ></input>
+      </div>
+
+      <div className="form-row pb-3">
+        <div className="col">
+          <div className="form-group">
+            <label className="btn btn-outline-primary text-left">
+              {values.loading ? "Uploading..." : "Select an Image to upload"}
+              <input
+                type="file"
+                name="image"
+                onChange={handleImage}
+                accept="image/*"
+                hidden
+              />
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="row pb-3">
+        <div className="col">
+          <Button
+            onClick={handleSubmit}
+            disabled={values.loading || values.uploading}
+            className="btn btn-primary btn-lg"
+            loading={values.loading}
+            type="primary"
+            size="large"
+            shape="round"
+          >
+            {values.loading ? "Saving..." : "Save & Continue"}
+          </Button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default CreateCourseForm;
--- pages/instructor/course/create.js
+++ pages/instructor/course/create.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import axios from "axios";
 import InstructorRoute from "../../../components/routes/InstructorRoute";
+import CreateCourseForm from "../../../components/forms/CreateCourseForm";
 
 const CreateCourse = () => {
+  const [values, setValues] = useState({
+    name: "",
+    description: "",
+    price: "9.99",
+    category: "",
+    uploading: false,
+    paid: true,
+    loading: false,
+    imagePreview: "",
+  });
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: [e.target.value] });
+  };
+  const handleImage = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.table(values);
+  };
+
   return (
     <InstructorRoute>
       <div className="jumbotron text-center">
         <h3 className="navTitle">Create Course</h3>
       </div>
+      <div style={{ width: "70%", margin: "0 auto", position: "relative" }}>
+        <CreateCourseForm
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+          handleImage={handleImage}
+          values={values}
+          setValues={setValues}
+        />
+      </div>
+      <pre>{JSON.stringify(values, null, 4)}</pre>
     </InstructorRoute>
   );
 };
