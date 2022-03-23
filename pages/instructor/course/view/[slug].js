f9e40b8645742c6f1e10ee4ecd4d597ae95b6210
Silvin Pradhan <silvinpradhan95@gmail.com>
2022-03-22 21:55:35 -0500

    built ui for single Course page with slug

--- pages/instructor/course/view/[slug].js
+++ pages/instructor/course/view/[slug].js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import InstructorRoute from "../../../../components/routes/InstructorRoute";
 import axios from "axios";
-import { Avatar, Tooltip } from "antd";
-import { EditOutlined } from "@ant-design/icons";
+import { Image, Tooltip } from "antd";
+import { CheckOutlined, EditOutlined } from "@ant-design/icons";
 import ReactMarkdown from "react-markdown";
 
 const CourseView = () => {
@@ -21,37 +21,56 @@ const CourseView = () => {
     <InstructorRoute>
       <div className="container-fluid pt-3">
         {course && (
-          <div className="container-fluid pt-1">
-            <div className="col pt-2">
-              <Avatar
-                size={150}
+          <div className="container">
+            <div className="mb-2">
+              <Image
+                width={250}
                 src={course.image ? course.image.Location : "/course.png"}
+                alt={course.name}
               />
-              <div className="px-2">
-                <div className="row">
-                  <div className="col">
-                    <h5 className="mt-2 text-primar">{course.name}</h5>
-                    <p style={{ marginTop: "-10px" }}>
-                      {course.lessons && course.lessons.length} Lessons
-                    </p>
-                    <p style={{ marginTop: "-150px", fontSize: "10px" }}>
-                      {course.category}
-                    </p>
-                  </div>
-                  <div className="d-flex pt-4">
-                    <Tooltip title="Edit">
-                      <EditOutlined className="h5 pointer text-warning mr-4" />
-                    </Tooltip>
-                    <Tooltip title="Publish">
-                      <EditOutlined className="h5 pointer text-danger mr-4" />
-                    </Tooltip>
-                  </div>
+            </div>
+
+            <div className="d-flex justify-content-between">
+              <div>
+                <h5
+                  className="mt-2 text-primary mb-3"
+                  style={{ fontSize: "20px" }}
+                >
+                  {course.name}
+                </h5>
+                <div className="d-flex justify-content-between">
+                  <p
+                    style={{ marginTop: "-10px", fontSize: "14px" }}
+                    className="badge bg-success"
+                  >
+                    {course.lessons && course.lessons.length} Lessons
+                  </p>
+                  <p
+                    style={{
+                      marginLeft: "5px",
+                      marginTop: "-10px",
+                      fontSize: "15px",
+                      fontStyle: "italic",
+                    }}
+                    className="badge bg-secondary"
+                  >
+                    #{course.category}
+                  </p>
                 </div>
               </div>
+              <div>
+                <Tooltip title="Edit" className="px-2">
+                  <EditOutlined className="h5 pointer text-warning mr-4" />
+                </Tooltip>
+                <Tooltip title="Publish" className="px-2">
+                  <CheckOutlined className="h5 pointer text-danger mr-4" />
+                </Tooltip>
+              </div>
             </div>
+            <hr />
             <div className="row">
               <div className="col">
-                <ReactMarkdown source={course.description} />
+                <ReactMarkdown children={course && course.description} />
               </div>
             </div>
           </div>
